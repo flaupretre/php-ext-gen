dea2b7e5cc174bf7115ab428e5ce8af8a965f751
Francois Laupretre <francois@tekwire.net>
2015-01-19 21:57:10 +0100

    Integrate module_globals template in main (contains blocks)

--- src/generators/php5/templates/main.twig.c
+++ src/generators/php5/templates/main.twig.c
@@ -15,9 +15,53 @@ zend_module_entry {{ name }}_module_entry;
 #define phpext_{{ name }}_ptr &{{ name }}_module_entry
 
 /*---------------------------------------------------------------*/
-/* Module global data */
+/*--- Module global data */
 
-{% include 'module_globals.twig.c' %}
+ZEND_BEGIN_MODULE_GLOBALS({{ name }})
+#ifdef HAVE_GETTIMEOFDAY
+struct timeval _eg_base_tp;
+#endif
+
+{% block user_module_globals_data %}{% endblock %}
+
+ZEND_END_MODULE_GLOBALS({{ name }})
+
+/*---*/
+
+#ifdef ZTS
+#	define {{ uname }}_G(v) TSRMG({{ name }}_globals_id, zend_{{ name }}_globals *, v)
+#else
+#	define {{ uname }}_G(v) ({{ name }}_globals.v)
+#endif
+
+ZEND_DECLARE_MODULE_GLOBALS({{ name }})
+
+#ifdef COMPILE_DL_{{ uname }}
+	ZEND_GET_MODULE({{ name }})
+#endif
+
+/*---------------------------------------------------------------*/
+/* Module global init */
+/* Initialize a new zend_{{ name }}_globals struct during thread spin-up */
+{# Default: clear memory block #}
+
+static void {{ name }}_globals_ctor(zend_{{ name }}_globals * globals TSRMLS_DC)
+{
+EG_CLEAR_DATA(*globals); /* Init everything to 0/NULL */
+
+{% block user_module_globals_init %}{% endblock %}
+}
+
+/*------------------------*/
+/* Module global destructor */
+/* Any resources allocated during initialization may be freed here */
+
+#ifndef ZTS
+static void {{ name }}_globals_dtor(zend_{{ name }}_globals * globals TSRMLS_DC)
+{
+{% block user_module_globals_dtor %}{% endblock %}
+}
+#endif
 
 /*---------------------------------------------------------------*/
 /* Fixed values */
@@ -25,14 +69,20 @@ zend_module_entry {{ name }}_module_entry;
 {% include 'fixed_values.twig.c' %}
 
 /*---------------------------------------------------------------*/
+/* extgen functions */
+
+{% include 'extgen.twig.c' %}
+
+/*---------------------------------------------------------------*/
 /*--- Header code */
 
 {{ global_data.header }}
 
 /*---------------------------------------------------------------*/
-/* extgen functions */
+/* Ini settings */
 
-{% include 'extgen.twig.c' %}
+{% include 'ini_settings.twig.c' %}
+{% block user_ini_settings %}{% endblock %}
 
 /*---------------------------------------------------------------*/
 /* Extension functions */
@@ -48,7 +98,87 @@ zend_module_entry {{ name }}_module_entry;
 /*---------------------------------------------------------------*/
 /* PHP_Mxxx functions */
 
-{% include 'php_mfunctions.twig.c' %}
+/*-------------------*/
+/* phpinfo() output  */
+
+static PHP_MINFO_FUNCTION({{ name }})
+{
+{% include 'common_minfo_code.twig.c' %}
+
+{% block user_minfo %}{% endblock %}
+}
+
+/*---------------------------------------------------------------*/
+
+static PHP_RINIT_FUNCTION({{ name }})
+{
+EG_DBG_INIT();
+
+{% block user_rinit %}{% endblock %}
+
+return SUCCESS;
+}
+
+/*---------------------------------------------------------------*/
+
+static PHP_RSHUTDOWN_FUNCTION({{ name }})
+{
+{% block user_rshutdown %}{% endblock %}
+
+return SUCCESS;
+}
+
+/*---------------------------------------------------------------*/
+
+static PHP_MINIT_FUNCTION({{ name }})
+{
+{% block user_minit_start %}{% endblock %}
+
+_eg_build_{{ name }}_constant_values();
+
+{% block user_minit_pre_init_globals %}{% endblock %}
+
+/* Init module globals */
+
+ZEND_INIT_MODULE_GLOBALS({{ name }}, {{ name }}_globals_ctor, NULL);
+
+{% block user_minit_post_init_globals %}{% endblock %}
+
+/* Declare constants */
+
+{% block user_minit_pre_constant_declare %}{% endblock %}
+
+{% for cname,const in constants %}
+	{% if (const.type=='null') %}
+		_EG_REGISTER_NULL_CONSTANT("{{ cname }}");
+	{% else %}
+		_EG_REGISTER_{{ const.type|upper }}_CONSTANT("{{ cname }}", {{ const.value }});
+	{% endif %}
+{% endfor %}
+
+{% block user_minit_post_constant_declare %}{% endblock %}
+
+/*--------*/
+
+{% block user_minit_end %}{% endblock %}
+
+return SUCCESS;
+}
+
+/*---------------------------------------------------------------*/
+
+static PHP_MSHUTDOWN_FUNCTION({{ name }})
+{
+{% block user_mshutdown_pre %}{% endblock %}
+
+#ifndef ZTS
+	{{ name }}_globals_dtor(&{{ name }}_globals TSRMLS_CC);
+#endif
+
+{% block user_mshutdown_post %}{% endblock %}
+
+return SUCCESS;
+}
 
 /*---------------------------------------------------------------*/
 /*-- Module definition --*/
